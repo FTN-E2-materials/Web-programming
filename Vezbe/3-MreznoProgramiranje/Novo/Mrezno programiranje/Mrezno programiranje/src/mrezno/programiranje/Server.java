615b612ed96bb7156cd1d325475daf8ac351ce0a
Pufke <pufke1998@gmail.com>
2020-03-18 16:34:30 +0100

    Taks 1 implemented class UserThread, that class extends classThread, implemented logic from method Server.handleRequest in method UserHtread.run

---
+++ Vezbe/3-MreznoProgramiranje/Novo/Mrezno programiranje/Mrezno programiranje/src/mrezno/programiranje/Server.java
@@ -0,0 +1,27 @@
+package mrezno.programiranje;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static final int TCP_PORT = 9000;
+
+	public static void main(String[] args) {
+		try {
+			int clientCounter = 0;
+			
+			// slusaj zahteve na datom portu
+			@SuppressWarnings("resource")
+			ServerSocket ss = new ServerSocket(TCP_PORT);
+			System.out.println("Server running...");
+			while (true) {
+				Socket sock = ss.accept();
+				System.out.println("Client accepted: " + (++clientCounter));
+				new UserThread(sock, clientCounter);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
