e0143542718fe4da13717d1db376937fa109a2f2
Pufke <pufke1998@gmail.com>
2020-03-19 18:56:04 +0100

    Finaly reorganization files

--- Vezbe/3-MreznoProgramiranje/Novo/Mrezno programiranje/Mrezno programiranje/src/mrezno/programiranje/Server.java
+++ Vezbe/3-MreznoProgramiranje/Novo/Mrezno programiranje/Mrezno programiranje/src/mrezno/programiranje/Server.java
@@ -1,4 +1,10 @@
 package mrezno.programiranje;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,7 +15,6 @@ public class Server {
 	public static void main(String[] args) {
 		try {
 			int clientCounter = 0;
-			
 			// slusaj zahteve na datom portu
 			@SuppressWarnings("resource")
 			ServerSocket ss = new ServerSocket(TCP_PORT);
@@ -17,8 +22,32 @@ public class Server {
 			while (true) {
 				Socket sock = ss.accept();
 				System.out.println("Client accepted: " + (++clientCounter));
-				new UserThread(sock, clientCounter);
+				handleReqeust(sock, clientCounter);
 			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	private static void handleReqeust(Socket sock, int value) {
+		try {
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(sock.getInputStream()));
+
+			// inicijalizuj izlazni stream
+			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+					sock.getOutputStream())), true);
+			// procitaj zahtev
+			String request = in.readLine();
+			System.out.println(request);
+
+			// odgovori na zahtev
+			out.println("(" + value + ")");
+
+			// zatvori konekciju
+			in.close();
+			out.close();
+			sock.close();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
--- Vezbe/3-MreznoProgramiranje/v3-resenje-Pufke/Mrezno programiranje/src/mrezno/programiranje/Server.java
+++ Vezbe/3-MreznoProgramiranje/v3-resenje-Pufke/Mrezno programiranje/src/mrezno/programiranje/Server.java
@@ -1,10 +1,4 @@
 package mrezno.programiranje;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,6 +9,7 @@ public class Server {
 	public static void main(String[] args) {
 		try {
 			int clientCounter = 0;
+			
 			// slusaj zahteve na datom portu
 			@SuppressWarnings("resource")
 			ServerSocket ss = new ServerSocket(TCP_PORT);
@@ -22,32 +17,8 @@ public class Server {
 			while (true) {
 				Socket sock = ss.accept();
 				System.out.println("Client accepted: " + (++clientCounter));
-				handleReqeust(sock, clientCounter);
+				new UserThread(sock, clientCounter);
 			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	private static void handleReqeust(Socket sock, int value) {
-		try {
-			BufferedReader in = new BufferedReader(
-					new InputStreamReader(sock.getInputStream()));
-
-			// inicijalizuj izlazni stream
-			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
-					sock.getOutputStream())), true);
-			// procitaj zahtev
-			String request = in.readLine();
-			System.out.println(request);
-
-			// odgovori na zahtev
-			out.println("(" + value + ")");
-
-			// zatvori konekciju
-			in.close();
-			out.close();
-			sock.close();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
