f9538f644a42c3107d5be81655ebb8dd2cbba163
vlaksi <daksrapsody@gmail.com>
2020-06-13 22:39:52 +0200

    Kostur modifikovan i uredno refaktorisan

---
+++ MiniProjekat/Vaksi/Kostur-MojSaModifikacijama/PocetniServleti/src/model/User.java
@@ -0,0 +1,38 @@
+package model;
+
+/**
+ * Predstavnica modela YYYYY
+ * @author Vaxi
+ *
+ */
+public class User {
+	
+	private String name;
+	private String username;
+	
+	public User() {
+		
+	}
+
+	public User(String name, String username) {
+		super();
+		this.name = name;
+		this.username = username;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+	
+}
